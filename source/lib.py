70e3183b0e5bd708c812c1935d38c72cbe0ea53c
Andrio <ask.lazer4@gmail.com>
2017-07-05 19:19:50 +0300

    dump

--- source/lib.py
+++ source/lib.py
@@ -1,23 +1,27 @@
 class OpCode:
-	def __init__(self, opcode, *args):
-		self.opcode = opcode
-		self.args = args
+    pass
 
+
+class SET(OpCode):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
 
 class Parser:
-	def __init__(self):
-		self.code = []
+    def __init__(self):
+        self.code = []
     
     def parse(self, line):
         if '=' in line:
-        	self.code.append()
+            name, value = line.split('=')
+            self.code.append(SET(name, value))
 
     @classmethod 
     def parse_file(cls, filename):
-    	obj = cls()
-    	for line in open(filename):
-    		obj.parse(line)
-    	return obj
+        obj = cls()
+        for line in open(filename):
+            obj.parse(line)
+        return obj
      
 
     
