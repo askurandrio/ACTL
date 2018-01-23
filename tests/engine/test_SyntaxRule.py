11ca0355e8f091e7627638fbd636ed48e5716824
askurandrio <ask.lazer4@gmail.com>
2018-01-23 23:53:33 +0200

    added test for SyntaxRule.Or

--- tests/engine/test_SyntaxRule.py
+++ tests/engine/test_SyntaxRule.py
@@ -3,18 +3,23 @@ import unittest
 
 from actl import Code
 from actl.parser import opcodes
-from actl.code.SyntaxRule import Or, SyntaxRules
+from actl.code.SyntaxRule import Or, Not, SyntaxRules
 
 
 class test_SyntaxRule(unittest.TestCase):
 	def init(self, code_variant=None):
 		rules = SyntaxRules()
 		code = Code(None, rules, None)
-		if code_variant == 'simple':
+		if (code_variant == 'simple') or (code_variant == 'or'):
 			code.buff = [opcodes.VARIABLE('a'),
 							 opcodes.VARIABLE('b'),
 							 opcodes.VARIABLE('c'),
 							 opcodes.VARIABLE('d')]
+		elif code_variant == 'not':
+			code.buff = [opcodes.VARIABLE('a'),
+							 opcodes.VARIABLE('b'),
+							 opcodes.VARIABLE('c'),
+							 opcodes.VARIABLE('b')]
 		return code, rules
 
 	def test_simple(self):
@@ -30,7 +35,7 @@ class test_SyntaxRule(unittest.TestCase):
 											  opcodes.VARIABLE('d')])
 
 	def test_or(self):
-		code, rules = self.init('simple')
+		code, rules = self.init('or')
 
 		@rules.add(Or((opcodes.VARIABLE('b'),), (opcodes.VARIABLE('c'),)))
 		def _(_, var1, var2):
@@ -40,3 +45,15 @@ class test_SyntaxRule(unittest.TestCase):
 		self.assertEqual(code.buff, [opcodes.VARIABLE('a'),
 											  opcodes.VARIABLE('Or(b, c)'),
 											  opcodes.VARIABLE('d')])
+
+	def test_not(self):
+		code, rules = self.init('not')
+
+		@rules.add(Not((opcodes.VARIABLE('a'),), (opcodes.VARIABLE('c'),)))
+		def _(_, var1, var2):
+			return (opcodes.VARIABLE(f'Not({var1.name}, {var2.name})'),)
+
+		code.compile()
+		self.assertEqual(code.buff, [opcodes.VARIABLE('a'),
+											  opcodes.VARIABLE('b'),
+											  opcodes.VARIABLE('Not(c, b)'),])
