f7f14cc5c9e4e0575b6ff83257e9fe0cb0a817d9
Andrio <ask.lazer4@gmail.com>
2021-04-13 15:37:46 +0300

    Fix setVariable

--- library/actl/syntax/Parsed.py
+++ library/actl/syntax/Parsed.py
@@ -7,9 +7,19 @@ class Parsed(AbstractTemplate):
 	__slots__ = ('until',)
 
 	def __init__(self, *templates):
-		super().__init__(Template(*templates))
+		if templates:
+			until = Template(*templates)
+		else:
+			until = None
+		super().__init__(until)
 
 	def __call__(self, parser, buff):
-		parser.subParser(buff.origin, self.until).parseLine()
+		subParser = parser.subParser(buff.origin, self.until)
+		if self.until is None:
+			subParser.parseLine(insertDefiniton=False)
+			parser.definition += subParser.definition
+			return ()
+
+		subParser.parseLine()
 		res = BufferRule(parser, buff).popUntil(self.until)
 		return tuple(res)
