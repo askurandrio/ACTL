2a613e10c6fe012dafc01ae5205696dfbce157d9
Andrio <ask.lazer4@gmail.com>
2021-06-12 21:40:33 +0300

    Going forward with pylint-ignore

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ set -e
 
 PYTHONPATH=./library py.test --cov=library --cov-report term-missing --cov-branch --no-cov-on-fail ./tests -x --cov-fail-under 93 -vv
 python3 -m coverage_badge -f -o coverage.svg
-PYTHONPATH=./library pylint library tests
+PYTHONPATH=./library pylint-ignore library tests
