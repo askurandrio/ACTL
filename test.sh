7320bbd46130ec9ea5d6d3b282cfb25f18c886f8
Andrio <ask.lazer4@gmail.com>
2022-04-22 08:10:37 +0200

    Use standart python for coverage bagde

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ set -e
 
 
 PYTHONPATH=./library py.test --cov=library --cov-report term-missing --cov-branch --no-cov-on-fail ./tests -x --cov-fail-under 94 -vv
-python3 -m coverage_badge -f -o coverage.svg
+python -m coverage_badge -f -o coverage.svg
 PYTHONPATH=./library pylint-ignore library tests
