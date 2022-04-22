60670d25198edc5780a8d7d997f71c456aee2ec7
Andrio <ask.lazer4@gmail.com>
2022-04-22 08:09:48 +0200

    Improve error about incorrect coroutine in executeSyncCoroutine

--- library/actl/objects/object/executeSyncCoroutine.py
+++ library/actl/objects/object/executeSyncCoroutine.py
@@ -3,10 +3,10 @@ def executeSyncCoroutine(coroutine):
 	generator = coroutine.__await__()
 
 	try:
-		next(generator)
+		opcode = next(generator)
 	except StopIteration as ex:
 		if ex.args:
 			return ex.args[0]
 		return None
 	else:
-		raise RuntimeError('Generator do not stopped')
+		raise RuntimeError(f'Generator do not stopped. Info<opcode={opcode}, coroutine={coroutine}>')
