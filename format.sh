1cd1d8c95f7527c7810ada714ffcba8c910dfcd5
Andrio <ask.lazer4@gmail.com>
2022-05-31 22:45:50 +0200

    Format .a files in format.sh

--- format.sh
+++ format.sh
@@ -4,7 +4,7 @@ set -e
 
 black -S . &> /dev/null
 
-for filename in $(find . -type f -name "*.py"); do
+for filename in $(find . -type f -name "*.py" -o -name "*.a"); do
 	sed -e 's/    /\t/g' ${filename} > ${filename}-tab
 	mv ${filename}-tab $filename
 done
