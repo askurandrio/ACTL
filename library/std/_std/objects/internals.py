347360a2bc03286c80ec75b2e35ad88d14e21884
Andrio S <ask.lazer4@gmail.com>
2022-08-09 16:11:34 +0000

    Add Iter class

--- library/std/_std/objects/internals.py
+++ library/std/_std/objects/internals.py
@@ -1,4 +1,5 @@
-from actl.objects import NativeFunction
+from actl.objects import NativeFunction, class_
+from actl import executeSyncCoroutine
 
 
 @NativeFunction
@@ -7,3 +8,17 @@ async def addMethod(cls, name, method):
 
 	self_ = await cls.getAttribute('__self__')
 	self_[name] = method
+
+
+Iter = executeSyncCoroutine(class_.call('Iter'))
+
+
+@Iter.addMethod('__init__')
+async def _Iter__init(self, collection):
+	head = await collection.getAttribute('_head')
+	self._head = iter(head._value)
+
+
+@Iter.addMethod('next')
+async def _Iter__next(self):
+	return next(self._head)
