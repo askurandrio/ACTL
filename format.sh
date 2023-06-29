3c3cad406581a3704ac5fb3b49778513770297ce
Andrio <ask.lazer4@gmail.com>
2023-06-29 16:13:21 +0200

    Improve works format.sh in case of error

--- format.sh
+++ format.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-black -S . &> /dev/null
+black -S . &> /dev/null || (set -ex; black -S .; exit 1)
 
 for filename in $(find . -type f -name "*.py" -o -name "*.a"); do
 	sed -e 's/    /\t/g' ${filename} > ${filename}-tab
