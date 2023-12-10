9ac87d59c8f168a63533d45537735e37a8d1429d
Andrio <ask.lazer4@gmail.com>
2023-12-10 09:04:03 +0100

    Improve test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 
-set -x
-set -e
+set -xe
 
-./format.sh
-PYTHONPATH=./library python -m pytest --cov=library --cov-report term-missing --cov-branch --no-cov-on-fail ./tests -x --cov-fail-under 94 -vv
-python -m coverage_badge -f -o coverage.svg
-PYTHONPATH=./library python -m pylint library tests
+
+if [[ "$1" == "format" ]]; then
+    ./format.sh
+    exit 0
+elif [[ "$1" == "pytest" ]]; then
+    PYTHONPATH=./library python -m pytest --cov=library --cov-report term-missing --cov-branch --no-cov-on-fail ./tests -x --cov-fail-under 93 -vv
+elif [[ "$1" == "pylint" ]]; then
+    PYTHONPATH=./library python -m pylint library tests
+else
+    ./test.sh format
+    ./test.sh pytest
+    python -m coverage_badge -f -o coverage.svg
+    ./test.sh pylint
+fi
