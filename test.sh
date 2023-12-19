277267ca29d99f9a78cc25a12769ddd2c7836a53
Andrio <ask.lazer4@gmail.com>
2023-12-19 22:09:54 +0100

    Add -k option to tests.sh

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ if [[ "$1" == "format" ]]; then
     exit 0
 elif [[ "$1" == "pytest" ]]; then
     PYTHONPATH=./library python -m pytest --cov=library --cov-report term-missing --cov-branch --no-cov-on-fail ./tests -x --cov-fail-under 93 -vv
+elif [[ "$1" == "-k" ]]; then
+    PYTHONPATH=./library python -m pytest ./tests -x -k $2 -vv
 elif [[ "$1" == "pylint" ]]; then
     PYTHONPATH=./library python -m pylint library tests
 else
